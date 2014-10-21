18fd391e0885ac5b6b83ccb140fc920fab48efd6
diqin1990 <diqin@Dis-MacBook-Pro.local>
2014-10-21 15:35:04 -0600

    Game Setup

---
+++ Unity_Project/Assets/Script/RoadController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadController : MonoBehaviour 
+{
+	public GameObject player;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			player.GetComponent<PlayerController>().jumpTime = 0;
+		}
+	}
+}
